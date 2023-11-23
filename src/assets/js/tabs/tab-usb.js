8bfaba6272549f90de8c1ea2a759f63ce4fd3f2a
Sven Rademakers <sven.rademakers@gmail.com>
2023-11-23 13:36:14 +0000

    Revert "added boot pin checkbox"
    This reverts commit 00f0c9c9ece35a6fa42d5e6f0f3494c4b5cfba58.

--- src/assets/js/tabs/tab-usb.js
+++ src/assets/js/tabs/tab-usb.js
@@ -1,5 +1,4 @@
-import {SetSessionNotification, showToastNotification} from "../functions/notifications.js";
-import {ajaxFailToast} from "../app.js"
+import {SetSessionNotification} from "../functions/notifications.js";
 
 $('#form-usb').parsley();
 
@@ -8,11 +7,9 @@ $("#form-usb").on('submit', function (e) {
 
     const form = $(this)
     const btn = form.find('button[type=submit]')
+
     const mode = $('#usbMode').val();
     const node = $('#usbNode').val();
-    const bootpin = $('#usbBoot').is(':checked');
-
-    toggle_usb_boot_pin(bootpin, node);
 
     var url = `/api/bmc?opt=set&type=usb&mode=${mode}&node=${node}`;
 
@@ -46,19 +43,3 @@ $("#form-usb").on('submit', function (e) {
     })
 
 });
-
-function toggle_usb_boot_pin(enabled, node) {
-    var usb_boot_url;
-    if (enabled) {
-        usb_boot_url = `/api/bmc?opt=set&type=usb_boot&node=${node}`;
-    } else {
-        usb_boot_url = `/api/bmc?opt=set&type=clear_usb_boot`;
-    }
-
-    $.ajax({
-        url: usb_boot_url,
-        type: 'GET',
-        timeout: 5000,
-        dataType: "json",
-    }).fail(ajaxFailToast);
-}
