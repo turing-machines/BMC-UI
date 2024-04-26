89c76fc735ec5fc79dc7e51fbeeba494f940bc8f
Sebastian Barrenechea <sebastian@barrenechea.cl>
2024-04-25 20:22:41 -0400

    Refactor CSS styles in _tabs-layout.scss and _login.scss files, add footer

--- src/routes/_tabLayout.tsx
+++ src/routes/_tabLayout.tsx
@@ -27,38 +27,46 @@ export const Route = createFileRoute("/_tabLayout")({
 function AppLayoutComponent() {
   return (
     <div className="app-wrapper">
-      <div className="app-header">
-        <div className="logo">
-          <Logo />
+      <div>
+        <div className="app-header">
+          <div className="logo">
+            <Logo />
+          </div>
+          <BasicInfo />
         </div>
-        <BasicInfo />
-      </div>
 
-      <div className="tabs-wrapper">
-        <div className="tabs-header tabs-header__list">
-          <Link to="/info" className="tabs-header__list-item">
-            Info
-          </Link>
-          <Link to="/nodes" className="tabs-header__list-item">
-            Nodes
-          </Link>
-          <Link to="/usb" className="tabs-header__list-item">
-            USB
-          </Link>
-          <Link to="/firmware-upgrade" className="tabs-header__list-item large">
-            Firmware Upgrade
-          </Link>
-          <Link to="/node-upgrade" className="tabs-header__list-item">
-            Flash Node
-          </Link>
-          <Link to="/about" className="tabs-header__list-item">
-            About
-          </Link>
+        <div className="tabs-wrapper">
+          <div className="tabs-header tabs-header__list">
+            <Link to="/info" className="tabs-header__list-item">
+              Info
+            </Link>
+            <Link to="/nodes" className="tabs-header__list-item">
+              Nodes
+            </Link>
+            <Link to="/usb" className="tabs-header__list-item">
+              USB
+            </Link>
+            <Link
+              to="/firmware-upgrade"
+              className="tabs-header__list-item large"
+            >
+              Firmware Upgrade
+            </Link>
+            <Link to="/node-upgrade" className="tabs-header__list-item">
+              Flash Node
+            </Link>
+            <Link to="/about" className="tabs-header__list-item">
+              About
+            </Link>
+          </div>
+          <div className="tabs-body">
+            <Outlet />
+          </div>
+          <ToastContainer position="bottom-right" stacked />
         </div>
-        <div className="tabs-body">
-          <Outlet />
-        </div>
-        <ToastContainer position="bottom-right" stacked />
+      </div>
+      <div className="copyright">
+        <p className="copyright__text">© TURING MACHINES INC.</p>
       </div>
     </div>
   );
--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -53,40 +53,47 @@ function Login() {
 
   return (
     <div className="login-wrapper">
-      <div className="login-form">
-        <h2 className="login-title">Login</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="username" className="input-wrap active">
-              <span className="label">Username</span>
-              <input type="text" id="username" name="username" />
-            </label>
-          </div>
-          <div className="form-group">
-            <label htmlFor="password" className="input-wrap active">
-              <span className="label">Password</span>
-              <input type="password" id="password" name="password" />
-            </label>
-          </div>
-          <div className="form-group form-flex-row">
-            <div className="checkbox-row form-flex-row">
-              <div className="checkbox-item">
-                <input type="checkbox" id="rememberMe" name="rememberMe" />
-              </div>
-              <label htmlFor="rememberMe" className="label">
-                Remember me
+      <div className="login-container">
+        <div className="login-form">
+          <h2 className="login-title">Login</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="username" className="input-wrap active">
+                <span className="label">Username</span>
+                <input type="text" id="username" name="username" />
               </label>
             </div>
-          </div>
-          <div className="form-group">
-            <button type="submit" className="btn btn-turing-small-yellow">
-              <span className="caption">Login</span>
-            </button>
-          </div>
-          <div className="form-group">
-            <p id="responseMessage">{message}</p>
-          </div>
-        </form>
+            <div className="form-group">
+              <label htmlFor="password" className="input-wrap active">
+                <span className="label">Password</span>
+                <input type="password" id="password" name="password" />
+              </label>
+            </div>
+            <div className="form-group form-flex-row">
+              <div className="checkbox-row form-flex-row">
+                <div className="checkbox-item">
+                  <input type="checkbox" id="rememberMe" name="rememberMe" />
+                </div>
+                <label htmlFor="rememberMe" className="label">
+                  Remember me
+                </label>
+              </div>
+            </div>
+            <div className="form-group">
+              <button type="submit" className="btn btn-turing-small-yellow">
+                <span className="caption">Login</span>
+              </button>
+            </div>
+            <div className="form-group">
+              <p className="error-message" id="responseMessage">
+                {message}
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div className="copyright-login">
+        <p className="copyright-login__text">© TURING MACHINES INC.</p>
       </div>
     </div>
   );
