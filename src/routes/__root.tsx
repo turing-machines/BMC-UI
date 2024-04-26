bbd8aefa55f0c5ee4393f8f93fca2de990118f77
Sebastian Barrenechea <sebastian@barrenechea.cl>
2024-04-26 10:59:14 -0400

    devtools to only be embedded in development runs

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,9 +2,8 @@
 import "../css/app.scss";
 import "react-responsive-modal/styles.css";
 
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { lazy, Suspense } from "react";
 
 import { type AuthContext } from "../contexts/AuthContext";
 
@@ -12,13 +11,33 @@ interface RouterContext {
   auth: AuthContext;
 }
 
+const ReactQueryDevtools = import.meta.env.PROD
+  ? () => null // Render nothing in production
+  : lazy(() =>
+      // Lazy load in development
+      import("@tanstack/react-query-devtools").then((res) => ({
+        default: res.ReactQueryDevtools,
+      }))
+    );
+
+const TanStackRouterDevtools = import.meta.env.PROD
+  ? () => null // Render nothing in production
+  : lazy(() =>
+      // Lazy load in development
+      import("@tanstack/router-devtools").then((res) => ({
+        default: res.TanStackRouterDevtools,
+      }))
+    );
+
 export const Route = createRootRouteWithContext<RouterContext>()({
   component: () => {
     return (
       <>
         <Outlet />
-        <ReactQueryDevtools />
-        <TanStackRouterDevtools initialIsOpen={false} />
+        <Suspense>
+          <ReactQueryDevtools />
+          <TanStackRouterDevtools initialIsOpen={false} />
+        </Suspense>
       </>
     );
   },
